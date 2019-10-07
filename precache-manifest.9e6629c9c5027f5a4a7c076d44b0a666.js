0bdcaa1e712ef09cac7d051d9465bc50fef0b1f6
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-07 19:42:32 +0300

    Updates

---
+++ precache-manifest.9e6629c9c5027f5a4a7c076d44b0a666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d11ad73e2e71af5691bb2932bc9722",
+    "url": "/Quotes-generator-app/index.html"
+  },
+  {
+    "revision": "a4e6fc399d564cffec23",
+    "url": "/Quotes-generator-app/static/css/main.60e50371.chunk.css"
+  },
+  {
+    "revision": "dfe74bc633c735c4e328",
+    "url": "/Quotes-generator-app/static/js/2.7b60ee13.chunk.js"
+  },
+  {
+    "revision": "a4e6fc399d564cffec23",
+    "url": "/Quotes-generator-app/static/js/main.bdb1cb22.chunk.js"
+  },
+  {
+    "revision": "56760251da4150d95b54",
+    "url": "/Quotes-generator-app/static/js/runtime-main.f50adf15.js"
+  }
+]);
